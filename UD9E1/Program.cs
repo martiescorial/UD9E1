afa3433d71aeee45fab7a452a6f98ad2c4702f34
Martina <martinaescorial9@gmail.com>
2021-07-03 14:28:06 +0200

    Add project files.

---
+++ UD9E1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+namespace UD9E1
+{
+    class Alumno
+    {
+        int edad;
+
+        string nombre;
+        public Alumno()
+        {
+            Console.WriteLine("Introduzca el nombre del alumno");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca la edad del alumno");
+            string linea = Console.ReadLine();
+            edad = int.Parse(linea);
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Nombre:" + nombre);
+            Console.WriteLine("Edad:" + edad);
+        }
+
+        public void MayorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine(nombre + " es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine(nombre + " no es mayor de edad.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.Mostrar();
+            alumno1.MayorEdad();
+            Console.ReadKey();
+        }
+    }
+}
